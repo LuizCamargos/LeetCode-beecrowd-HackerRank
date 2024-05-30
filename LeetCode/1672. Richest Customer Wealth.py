825539feef83bbc58b6c8c6bb1de91858a743a03
Luiz Camargos <46843342+LuizCamargos@users.noreply.github.com>
2024-05-29 21:04:04 -0300

    Create 1672. Richest Customer Wealth.py

---
+++ LeetCode/1672. Richest Customer Wealth.py
@@ -0,0 +1,17 @@
+'''
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+'''
+
+from typing import List
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        wealth = []
+        for account in accounts:
+            wealth.append(sum(account))
+        return max(wealth)
+
+#testing
+result = Solution().maximumWealth(accounts =[[2,8,7],[7,1,3],[1,9,5]])
+print(result)
